e119acbe9f531b8454af05083a523b5816fc2e71
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-06-19 00:21:01 +0600

    Add and delete api created

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Runtime array to store products
+let products = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// GET all products
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+// GET a specific product by ID
+app.get("/products/:id", (req, res) => {
+  const productId = Number(req.params.id);
+  const product = products.find((p) => p.id === productId);
+
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  res.json(product);
+});
+
+// POST a new product
+app.post("/products", (req, res) => {
+  const newProduct = req.body;
+  products.push(newProduct);
+  res.status(201).json(newProduct);
+});
+
+// DELETE a product by ID
+app.delete("/products/:id", (req, res) => {
+  const productId = Number(req.params.id);
+  const productIndex = products.findIndex((p) => p.id === productId);
+
+  if (productIndex === -1) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  const deletedProduct = products.splice(productIndex, 1)[0];
+
+  res.json(deletedProduct);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
