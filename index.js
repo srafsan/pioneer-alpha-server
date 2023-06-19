45b1cdbc13cc486abb9bf4f151f7de551eeb89d3
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-06-20 03:50:06 +0600

    MongoDB integrated to the CRUD operation

--- index.js
+++ index.js
@@ -1,91 +1,125 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Runtime array to store products
-let products = [];
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jqpj64k.mongodb.net/?retryWrites=true&w=majority`;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-// GET all products
-app.get("/products", (req, res) => {
-  res.json(products);
-});
-
-// GET a specific product by ID
-app.get("/products/:id", (req, res) => {
-  const productId = Number(req.params.id);
-  const product = products.find((p) => p.id === productId);
-
-  if (!product) {
-    return res.status(404).json({ error: "Product not found" });
-  }
-
-  res.json(product);
-});
-
-// POST a new product
-app.post("/products", (req, res) => {
-  const newProduct = req.body;
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-});
-
-// PUT (update) a product by ID
-app.put("/products/:id", (req, res) => {
-  const productId = Number(req.params.id);
-  const updatedProduct = req.body;
-
-  const productIndex = products.findIndex((p) => p.id === productId);
-
-  if (productIndex === -1) {
-    return res.status(404).json({ error: "Product not found" });
-  }
-
-  products[productIndex] = updatedProduct;
-
-  res.json(updatedProduct);
-});
-
-// DELETE a product by ID
-app.delete("/products/:id", (req, res) => {
-  const productId = Number(req.params.id);
-  const productIndex = products.findIndex((p) => p.id === productId);
-
-  if (productIndex === -1) {
-    return res.status(404).json({ error: "Product not found" });
-  }
-
-  const deletedProduct = products.splice(productIndex, 1)[0];
-
-  res.json(deletedProduct);
-});
-
-// Payment
-app.post("/create-payment-intent", async (req, res) => {
-  const { amount } = req.body;
-
+async function run() {
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: "usd",
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // Runtime array to store products
+    let products = [];
+
+    const productsCollection = client
+      .db("Pioneer_Alpha")
+      .collection("products");
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
     });
 
-    res.json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    console.error("Error creating payment intent:", error);
-    res.status(500).json({ error: "Failed to create payment intent" });
+    // GET all products
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+    // GET a specific product by ID
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const query = { _id: new ObjectId(id) };
+        const result = await productsCollection.findOne(query);
+        res.send(result);
+      } catch (error) {
+        res.status(400).send("Invalid ID");
+      }
+    });
+
+    // POST a new product
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+
+      res.send(result);
+    });
+
+    // PUT (update) a product by ID
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const coffee = {
+        $set: {
+          amount: updatedProduct.amount,
+        },
+      };
+
+      const result = await productsCollection.updateOne(
+        filter,
+        coffee,
+        options
+      );
+
+      res.send(result);
+    });
+
+    // DELETE a product by ID
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
+    // Create Payment Intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { amount } = req.body;
+      const price = amount * 1000;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: price,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
-});
+}
+run().catch(console.dir);
 
 // Start the server
 app.listen(PORT, () => {
